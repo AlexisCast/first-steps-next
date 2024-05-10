42d94e167241f93fbb8c0d8834ce6163a415cbd3
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-05-10 14:32:46 -0600

    implement metadata

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "SEO Title",
+	description: "SEO Description",
+	keywords: ["About Page", "AlexisCast", "information"],
+};
+
 export default function AboutPage() {
 	return (
 		<>
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Contact page",
+	description: "This is the contact page of the business",
+};
+
 export default function ContactPage() {
 	return (
 		<>
