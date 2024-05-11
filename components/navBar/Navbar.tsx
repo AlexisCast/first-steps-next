34e5dd618eb6b84ed006ac276369f597d70ac274
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-05-11 15:16:16 -0600

    replace a tags with Next/Link tags for navigation

--- components/navBar/Navbar.tsx
+++ components/navBar/Navbar.tsx
@@ -1,22 +1,28 @@
-import React from "react";
+import { HomeIcon } from "@primer/octicons-react";
+import Link from "next/link";
+import { text } from "stream/consumers";
+
+const navItems = [
+	{ path: "/about", text: "About" },
+	{ path: "/pricing", text: "Pricing" },
+	{ path: "/contact", text: "Contact" },
+];
 
 export const Navbar = () => {
 	return (
 		<nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-			<span className="m-2">Home</span>
+			<Link className="flex items-center" href={"/"}>
+				<HomeIcon className="m-2" />
+				<span>Home</span>
+			</Link>
 
 			<div className="flex flex-1"></div>
 
-			{/* TODO//will update navigation */}
-			<a className="m-2" href="/about">
-				About
-			</a>
-			<a className="m-2" href="/pricing">
-				Pricing
-			</a>
-			<a className="m-2" href="/contact">
-				Contact
-			</a>
+			{navItems.map((item) => (
+				<Link key={item.text} className="m-2" href={item.path}>
+					{item.text}
+				</Link>
+			))}
 		</nav>
 	);
 };
